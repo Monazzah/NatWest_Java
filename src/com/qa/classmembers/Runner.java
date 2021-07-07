4b8417ba9c78b2586fcf097e81fc60b6383e9c5b
Naz <monazzah.dar@outlook.com>
2021-07-07 12:08:41 +0100

    practice exercises

--- src/com/qa/classmembers/Runner.java
+++ src/com/qa/classmembers/Runner.java
@@ -1,29 +1,23 @@
 package com.qa.classmembers;
 
 public class Runner {
+	public static int phys;
+	public static int chem;
+	public static int bio;
+	public static int total;
 	
 public static void main(String[] args) {
-	int Phys       = 148;
-	int Chem       = 100;
-	int Bio        = 130;
-	int Total      = Phys + Chem + Bio;
+	phys       = 148;
+	chem       = 100;
+	bio        = 130;
+	total      = phys + chem + bio;
 	int Percentage = Total * 100/450;
-	String message1= "Physics mark:";
-	String message2= "Chemistry mark:";
-	String message3= "Biology mark:";
-	String message4= "Total:";
-	String message5= "Percentage:";
-	
-		System.out.print(message1);System.out.println(Phys);
-		System.out.print(message2);System.out.println(Chem);
-		System.out.print(message3);System.out.println(Bio);
-		System.out.print(message4);System.out.println(Total);
-		System.out.print(message5);System.out.println(Percentage);
-	
-	
-			
-	
-	
+	display();
+	}
+
+public static void display () {
+	System.out.println("Result in Physics:"+ phys+" Result in Chemistry:"+
+			 chem+ " Result in Biology:"+ bio+" Total: "+ phys + chem + bio );
 }
 
 }
